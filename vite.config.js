92a712736a0e92838ff3dc83f1578a983a50cde2
NidhiSharma <nidhisharma_cse21@ggct.co.in>
2025-01-07 23:12:52 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // Generate source maps for easier debugging
+    logLevel: 'info', // More verbose output
+  },
 })
+
